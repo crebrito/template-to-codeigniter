851f0915e93326dc43b85a9137032f81b9137bf6
Carlos Brito <crebrito@gmail.com>
2023-06-17 22:59:35 -0400

    Fin Video #7

--- public/assets/js/loadHomeData.js
+++ public/assets/js/loadHomeData.js
@@ -2,18 +2,23 @@ import {swipe} from '/assets/js/swipebanner.js'
 
 async function loadHomeData(){
   var bannerHome = document.querySelector('._bannerHome');
-  var trending = document.querySelector('._trending');
+  var recent = document.querySelector('._recent');
 
   const responses = await Promise.all([
     await fetch('/banner/home',{method:'get'}),
-    await fetch('/trendings',{method:'get'})
+    await fetch('/trendings',{method:'get'}),
+    await fetch('/recent',{method:'get'})
   ])
 
   
   const bannerHtml = await responses[0].text();
   const trendingHtml = await responses[1].text();
+  const recentHtml = await responses[2].text();
 
   bannerHome.innerHTML = bannerHtml;
+  recent.innerHTML = recentHtml;
+  
+  var trending = document.querySelector('._trending');
   trending.innerHTML = trendingHtml;
 
   // // !! Load banner home
